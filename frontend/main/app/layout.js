57bf36f05836893a039369e2a200a7b114324ace
KwakJakyeong <ggyul847@gmail.com>
2025-08-16 03:56:01 +0900

    add : 배포 테스트 문구

---
+++ frontend/main/app/layout.js
@@ -0,0 +1,10 @@
+// app/page.js
+
+export default function HomePage() {
+    return (
+      <main style={{ minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>🚧 테스트 중입니다 🚧</h1>
+      </main>
+    );
+}
+  
