6c4a9bf175211fe634b3b78714483b802d016c96
KwakJakyeong <ggyul847@gmail.com>
2025-08-16 07:46:52 +0900

    fix : hero section 크기 무시현상

--- frontend/main/components/sections/SectionHero.js
+++ frontend/main/components/sections/SectionHero.js
@@ -1,12 +1,10 @@
-
-export default function SectionHero(){
-    const styles = require("./SectionHero.module.css");
-    return(
-        <section id="home" className={`secton ${styles.wrap}`}>
-            <div>
-                <h1 className={styles.title}>Hero 섹션</h1>
-                <p className={styles.subtitle}>첫 화면 카피/CTA 영역</p>
-            </div>
-        </section>
-    );
+export default function SectionHero() {
+  return (
+    <section id="home" className="section">
+      <div>
+        <h1>Hero 섹션</h1>
+        <p>첫 화면 카피/CTA 영역</p>
+      </div>
+    </section>
+  )
 }
