642a009ce63f258628d5a83a40bde95da0fa1432
KwakJakyeong <ggyul847@gmail.com>
2025-08-19 01:39:14 +0900

    fix : nav 이름

--- frontend/main/config/sections.js
+++ frontend/main/config/sections.js
@@ -1,6 +1,6 @@
 export const sectionsConfig = [
   { key: 'hero', anchor: 'hero', label: 'home', navFlag: false },
-  { key: 'about', anchor: 'about', label: '센터소개', navFlag: true },
+  { key: 'about', anchor: 'about', label: '센터 안내', navFlag: true },
   { key: 'intro', anchor: 'intro', label: '전문가 소개', navFlag: true },
 ]
 
