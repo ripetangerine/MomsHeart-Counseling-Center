048ccad1c887d8759834fbe8cd5c13a4727a75ea
KwakJakyeong <ggyul847@gmail.com>
2025-09-11 23:56:03 +0900

    chore : SEO metadata 추가

--- frontend/main/lib/seo.js
+++ frontend/main/lib/seo.js
@@ -1,19 +1,49 @@
-export const metadata = {
-  metadataBase: new URL("https://momshearts.co.kr"),
-// www.momshearts.co.kr
+export default metadata = {
+  metadataBase: new URL("https://momshearts.co.kr"), // 기본 도메인
   title: {
-    default: "엄마마음심리상담센터",
-    template: "%s | 상담센터 이름",
+    default: "Mom's Hearts",
+    template: "%s | Mom's Hearts",
   },
-  description: "엄마마음심리상담센터 강서구 심리상담센터 성인,아동청소년,가족상담 및 종합심리검사, 상담교육 우장산역 심리상담센터 ",
+  description: "",
+  keywords: ["상담", "심리", "마음", "강서구", "상담센터", "심리상담", "엄마마음"],
   openGraph: {
     title: "엄마마음심리상담센터",
-    description: "상담센터 소개 및 상담 안내",
+    description: "",
     url: "https://momshearts.co.kr",
     siteName: "엄마마음심리상담센터",
-    images: ["/non/represent.jpg"],
+    images: [
+      {
+        url: "/non/represent.jpg",
+        width: 1200,
+        height: 630,
+        alt: "메인이미지",
+      },
+    ],
     locale: "ko_KR",
     type: "website",
   },
-  alternates: { canonical: "https://momshearts.co.kr" },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mom's Hearts",
+    description: "엄마들의 마음을 위한 상담 센터 소개 페이지",
+    images: ["/non/represent.jpg"],
+  },
+  alternates: {
+    canonical: "https://momshearts.co.kr",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  icons: {
+    icon: "/non/represent.jpg",
+  },
+  verification: {
+    google: "구글-서치콘솔-코드",
+    other: { "naver-site-verification": "네이버-코드" },
+  },
 };
